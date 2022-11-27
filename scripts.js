2339e1177206dbde3a0ac63974881ccfd915d509
PJ <jaminski@o2.pl>
2022-11-27 16:33:12 +0100

    NEXT

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 //---PARALLAX EFFECTS FOR THE BACKGROUND IMAGES---
-var homeBg = document.getElementById('home');
-window.addEventListener('scroll', function() {
-    homeBg.style.opacity = 1 - window.pageYOffset / 850 + ""
-    homeBg.style.top = +window.pageYOffset + "px"
-    homeBg.style.top = +window.pageYOffset/2 + "px"
-})
+// var homeBg = document.getElementById('home');
+// window.addEventListener('scroll', function() {
+//     homeBg.style.opacity = 1 - window.pageYOffset / 610 + ""
+//     homeBg.style.top = +window.pageYOffset + "px"//do usunieca?
+//     homeBg.style.top = +window.pageYOffset/2 + "px"
+// })
+// var aboutBg = document.getElementById('about-us');
+// window.addEventListener('scroll', function() {
+//     aboutBg.style.opacity = 3 - window.pageYOffset / 600 + ""
+//     aboutBg.style.top = +window.pageYOffset + "px"
+//     aboutBg.style.top = +window.pageYOffset/2 + "px"
+// })
+// var servicesBg = document.getElementById('services');
+// window.addEventListener('scroll', function() {
+//     servicesBg.style.opacity = 3 - window.pageYOffset / 1550 + ""
+//     servicesBg.style.top = +window.pageYOffset + "px"
+//     servicesBg.style.top = +window.pageYOffset/2 + "px"
+// })
+
+
 //---COUNTING SCRIPT USED IN ABOUT SECTION---
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 4000;
@@ -22,4 +36,30 @@ valueDisplays.forEach((valueDisplay) => {
             clearInterval(counter)
         }
     }, duration);
+})
+
+//--- OWL-CAROUSEL SCRIPT USED IN TESTIMONIAL SECTION---
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    responsive : {
+        // breakpoint from 0 up
+        0 : {
+            items : 1,
+            nav : false,
+        },
+        // breakpoint from 600 up
+        600 : {
+            items : 2,
+            nav : false,
+        },
+        // breakpoint from 968 up
+        968 : {
+            items : 3,
+            nav : false,
+            loop : false
+        }
+    }
 })
